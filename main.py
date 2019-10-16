8f3737d94686b1acf3842a857a4e87ff4ce56218
HwangToeMat <46777602+HwangToeMat@users.noreply.github.com>
2019-10-16 09:36:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from SRGAN import G_Net, D_Net
-from utils import Train, save_checkpoint
 from LossFunction import GeneratorLoss, DiscriminatorLoss
 from dataset_h5 import Read_dataset_h5
 import pytorch_ssim
@@ -72,20 +71,58 @@ def main():
         D_Loss = D_Loss.cuda() # set model&loss for use gpu
 
     print("===> Setting Optimizer")
+    G_optim = optim.Adam(G_Net.parameters())
+    D_optim = optim.Adam(D_Net.parameters())
+
     if opt.pretrained:
         if os.path.isfile(opt.pretrained):
             G_optim.load_state_dict(checkpoint['G_optim_state_dict'])
             D_optim.load_state_dict(checkpoint['D_optim_state_dict'])
-    else:
-        G_optim = optim.Adam(G_Net.parameters())
-        D_optim = optim.Adam(D_Net.parameters())
+            print("===> Setting Pretrained Optimizer")
 
     print("=> start epoch '{}'".format(epoch))
     print("===> Training")
     for epoch_ in range(epoch, opt.nEpochs + 1):
-        print("Start epoch {} ##########################################".format(epoch_))
-        Train(training_data_loader, G_Net, D_Net , G_optim, D_optim, G_Loss, D_Loss, epoch_)
-        save_checkpoint(epoch_, G_Net, D_Net , G_optim, D_optim)
+        print("===>  Start epoch {} #################################################################".format(epoch_))
+        G_Net.train()
+        D_Net.train()
+        for _, (input, label) in enumerate(training_data_loader):
+            HR = Variable(label)/255
+            LR = Variable(input)/255
+            if torch.cuda.is_available():
+                HR = HR.cuda()
+                LR = LR.cuda()
+            fake_img = G_Net(LR)
+
+            # Train Discriminator model
+            D_Net.zero_grad()
+            real_rate = D_Net(HR)
+            fake_rate = D_Net(fake_img)
+            d_loss = D_Loss(fake_rate, real_rate)
+            d_loss.backward(retain_graph=True)
+            D_optim.step()
+
+            # Train Generator model
+            G_Net.zero_grad()
+            g_loss = G_Loss(fake_rate, fake_img, HR)
+            g_loss.backward()
+            G_optim.step()
+
+            # loss 출력
+            if _%10 == 0:
+                print("===> Epoch[[{}]({}/{})]: D_Loss : {:.10f}, G_Loss : {:.10f}, SSIM : {:.10f}".format(epoch_, _, len(training_data_loader), d_loss, g_loss, pytorch_ssim.ssim(HR, fake_img)))
+
+        model_out_path = "checkpoint/" + "SRGAN_Adam_epoch_{}.tar".format(epoch_)
+        if not os.path.exists("checkpoint/"):
+            os.makedirs("checkpoint/")
+        torch.save({
+                'epoch': epoch_,
+                'G_Net_state_dict': G_Net.state_dict(),
+                'D_Net_state_dict': D_Net.state_dict(),
+                'G_optim_state_dict': G_optim.state_dict(),
+                'D_optim_state_dict': D_optim.state_dict()
+                }, model_out_path)
+        print("Checkpoint has been saved to the {}".format(model_out_path))
 
 if __name__ == "__main__":
     main()
